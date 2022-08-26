c33fb3860228f43d9dd064516e5f985a939f2659
TaiwoJazz <babsman4all@gmail.com>
2022-08-26 02:59:48 +0100

    functioning

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-const displayValue = document.querySelectorAll('.val');
+
 const display = document.querySelector('.calculator__display');
-let deleteNumber = document.querySelector('.delete');
-let clear = document.querySelector('.clear');
+const deleteNumber = document.querySelector('.delete');
+const clear = document.querySelector('.clear');
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+const equal = document.querySelector('.equal');
 
 let numbers = [];
+let currentOperator = [];
 
-deleteNumber.addEventListener('click', backspaceDisplay)
-clear.addEventListener('click', clearDisplay)
 
-displayValue.forEach(val => {
-  val.addEventListener('click', function(e) {
-    let x = e.currentTarget.value
-    numbers.push(x);
-    updateDisplay();
-  })
-});
+deleteNumber.addEventListener('click', backspaceDisplay);
+clear.addEventListener('click', clearDisplay);
 
 const updateDisplay = () => {
   display.textContent = numbers.join('')
@@ -25,11 +22,66 @@ const updateDisplay = () => {
 
 function backspaceDisplay() {
   numbers.pop()
+  currentOperator.pop();
   updateDisplay();
 }
 
 function clearDisplay() {
   numbers = [];
-  updateDisplay(); 
+  currentOperator = [];
+  updateDisplay();
 }
 
+function test1() {
+  digits.forEach(num => {
+    num.addEventListener('click', function(e) {
+      let x = e.currentTarget.value
+      numbers.push(x)
+      updateDisplay();
+    })
+  })
+}
+test1();
+
+equal.addEventListener('click', equalto)
+function equalto () {
+  let num2 = Number(numbers.join(''));
+
+  let operator = currentOperator
+
+  if (operator == '+') {
+    let ans = num1 + num2
+    display.textContent = ans
+    numbers = [];
+    numbers.push(ans)
+  } else if (operator == '-') {
+    let ans = num1 - num2
+    display.textContent = ans
+    numbers = [];
+    numbers.push(ans)
+  } else if (operator == '*') {
+    let ans = num1 * num2
+    display.textContent = ans
+    numbers = [];
+    numbers.push(ans)
+  } else if (operator == '÷') {
+    let ans = num1 / num2
+    display.textContent = ans
+    numbers = [];
+    numbers.push(ans)
+  } else {
+    display.textContent = 'Error'
+  }
+  currentOperator = [];
+}
+  
+operators.forEach(item => {
+  item.addEventListener('click', items)
+  function items(e) {
+    let x = e.currentTarget.value
+    currentOperator.push(x)
+    window.num1 = Number(numbers.join(''));
+    display.textContent = currentOperator.join('')
+    numbers = [];
+  }
+});
